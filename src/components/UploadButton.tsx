3b725f241ed4b8356e8d2c98bbb75c02583db02f
OmarZen <omarwaleedzenhom2002@gmail.com>
2025-01-15 16:32:52 +0200

    setup database - start in Dashboard

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import { Dialog } from "./ui/dialog"
+import { DialogTrigger } from "@radix-ui/react-dialog"
+import { Button } from "./ui/button"
+
+const UploadButton = () => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    return (
+        <Dialog open={isOpen} onOpenChange={(v) => {
+            if (!v) {
+                setIsOpen(false)
+            }
+        }}>
+            <DialogTrigger asChild>
+                <Button>Upload PDF</Button>
+            </DialogTrigger>
+        </Dialog>
+    )
+
+}
+export default UploadButton
